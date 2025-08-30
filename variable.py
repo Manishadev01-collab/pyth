afc77fcfffeaebf2e24e5db67a6316c935ed9b84
Manisha <manisha.missingskill@gmail.com>
2025-08-30 15:42:54 +0530

    this is my second commit

---
+++ variable.py
@@ -0,0 +1,70 @@
+
+# Our First program
+print ("Hello world")
+
+
+name = "Manisha" #string
+age = 23 # Number
+age2=age
+
+price = 10  # Numbers
+rating = 4.9 # float 
+course_name = 'Python for beginners' # String 
+
+birth_year = int(input('Birth year:'))
+print(birth_year)
+
+
+
+
+
+
+
+print(age2)
+price = 23.99 # float number 
+print(name) 
+print("My name is :", name)
+print('My age is :', age)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+# datatype 
+# integers string float boolean none
+
+# string 
+name1 ="Sk"
+name2 = 'ssk'
+name3 = """ssk
+"""
+print(name1)
+print(name2)
+print(name3)
+
+age = 23
+old = False
+a = None
+
+print(type(old))
+print(type(a))
+
+# PRINT SUM 
+a = 2
+b = 5
+sum = a+b
+print(sum)
+num1 = 5
+num2 = 4
+diff= num1-num2
+print(diff)
+
+
+
+
+
+
+
+
+
+
