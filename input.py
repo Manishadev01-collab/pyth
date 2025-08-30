e2b0a3fb904064c2b5a115db90ae51b9d11127eb
Manisha <manisha.missingskill@gmail.com>
2025-08-30 16:21:25 +0530

    this is input file

---
+++ input.py
@@ -0,0 +1,38 @@
+# input statement is used to accept values (using keyword) from user
+
+# string input 
+
+# name = input("name:")
+
+
+# # int input
+
+# age = int (input("age:"))
+
+# # float input 
+# price = float(input("Price"))
+
+
+
+
+name = input("name:")
+age = int(input("age:"))
+price = int (input ("price:"))
+
+
+
+print("My name is ",name, "and I am ", age, "years old")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
