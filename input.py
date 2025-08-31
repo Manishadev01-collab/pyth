a9ab21735e1ba13cdaf71c5390e37dfdd593a793
Manisha <manisha.missingskill@gmail.com>
2025-08-31 14:39:24 +0530

    the operator commit

--- input.py
+++ input.py
@@ -36,3 +36,8 @@ print("My name is ",name, "and I am ", age, "years old")
 
 
 
+
+
+
+
+
---
+++ operator.py
@@ -0,0 +1,58 @@
+# a = 5
+# b=  2
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b)
+
+
+# # Comparison operators relational operators
+# a = 50
+# b = 20
+# print(a == b) # False
+# print(a !=b)  # True
+# print(a>=b)   # True
+# print(a >b)   # True
+# print(a<=b)   # True
+# print(a<b)    # False
+
+
+# Assignment operators
+
+# num = 10
+# num = num + 10
+# print("num :",num)
+
+# num = 10
+# num **= 5
+# print("num":num) # 1,00,000
+
+# Logical operators
+# not operator
+a= 50
+b = 30
+print(not False)
+print(not True)
+print(not (a>b))
+
+# and operator
+# ans true tabhi aata hai jab dono values true hoti hai 
+val1 = True
+val2 = False
+print("and operator:", val1 and val2)
+print("or operator:",val1 and val2)
+print("or operator:",(a ==b) or (a>b))
+
+
+
+
+
+
+
+
+
+
+
+
+
