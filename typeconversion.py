3149b04fe6814f671557e5a5b447c8c939a10bd9
Manisha <manisha.missingskill@gmail.com>
2025-08-31 14:49:46 +0530

    typeconversion

---
+++ typeconversion.py
@@ -0,0 +1,21 @@
+# type conversion
+a= 2
+b = 4.25
+sum = a + b # 2.0 + 4.25 = 6.25
+print(sum)
+
+a = "2"
+b = 4.25
+print(type(a))
+print(type(b))
+
+
+a = float("2")
+b = 4.25
+print(type(a))
+print(type(b))
+
+
+
+
+
